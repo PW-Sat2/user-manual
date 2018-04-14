8b2be422d5bce91e06b959416039b4214fc47d79
Novakov <maciejt.nowak@gmail.com>
2018-04-14 21:40:44 +0200

    Docs for set bitrate TC

--- helpers.py
+++ helpers.py
@@ -1,2 +1,10 @@
 def dump_tc(tc):
     print ' '.join([format(x, '02X') for x in tc.build()])
+
+def dump_ctor(tc):
+    import inspect
+    args = inspect.getargspec(tc.__init__)
+    names = args.args[1:]
+    names = map(lambda x: '<{}>'.format(x), names)
+    joined = ', '.join(names)
+    print '{}({})'.format(tc.__name__, joined).replace('_', '\_')
