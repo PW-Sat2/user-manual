573c2d05ed885f104231988670d7967134535111
Maciej Nowak <maciejt.nowak@gmail.com>
2018-05-04 19:58:57 +0200

    Updated GetCompileInfo telecommand

--- helpers.py
+++ helpers.py
@@ -1,18 +1,18 @@
 def dump_tc(tc):
     print ' '.join([format(x, '02X') for x in tc.build()])
 
-def dump_ctor(tc):
+def dump_ctor(telecommandType):
     import inspect
-    args = inspect.getargspec(tc.__init__)
+    args = inspect.getargspec(telecommandType.__init__)
     names = args.args[1:]
     names = map(lambda x: '<{}>'.format(x), names)
     joined = ', '.join(names)
-    print '{}({})'.format(tc.__name__, joined).replace('_', '\_')
+    print '{}({})'.format(telecommandType.__name__, joined).replace('_', '\_')
 
-def get_uplink_apid(tc):
-    x = tc.__new__(tc)
+def get_uplink_apid(telecommandType):
+    x = telecommandType.__new__(telecommandType)
     print('0x%02x' % x.apid())
 
-def get_downlink_apid(tc):
-    x = tc.__new__(tc)
+def get_downlink_apid(responseFrameType):
+    x = responseFrameType.__new__(responseFrameType)
     print('0x%02x' % x.ReceivedAPID)
